9cd71406aee4500349ce902a84267d562238bc5d
tinmanmusicman <tinmanmusicman@yahoo.com>
2026-01-31 22:00:19 -0800

    SARes V2.0.0 Color

--- ai-bots/SARes/App.xaml.cs
+++ ai-bots/SARes/App.xaml.cs
@@ -1,7 +1,25 @@
+using System.Threading.Tasks;
 using System.Windows;
+using SARes.ui;
 
 namespace SARes;
 
 public partial class App : System.Windows.Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        var splash = new SplashWindow();
+        splash.Show();
+        _ = ShowMainWindowAsync(splash);
+    }
+
+    private static async Task ShowMainWindowAsync(SplashWindow splash)
+    {
+        await Task.Delay(5000);
+        var main = new MainWindow();
+        Current.MainWindow = main;
+        main.Show();
+        splash.Close();
+    }
 }
